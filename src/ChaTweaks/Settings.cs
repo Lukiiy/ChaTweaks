1dfe8f0193cc8d950ce18db02d57f22f233bca4e
Lukiiy <lukiiy@proton.me>
2026-05-24 15:43:24 -0300

    label issues
    * Fixed labels not persisting

--- src/ChaTweaks/Settings.cs
+++ src/ChaTweaks/Settings.cs
@@ -1,13 +1,14 @@
 using HarmonyLib;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Localization.Components;
 
 namespace ChaTweaks;
 
 [HarmonyPatch]
 public static class ModSettings
 {
-    private static List<string> boolSetting = ["On", "Off"];
+    private static readonly List<string> boolSetting = ["On", "Off"];
 
     private static readonly (string label, string description, Func<bool> get, Action<bool> set)[] Settings = [
         ("Persistent Text Chat", "Toggle message persistency for the text chat", () => History.persistencyToggle, v => History.persistencyToggle = v),
@@ -26,25 +27,27 @@ public static class ModSettings
 
     private static void AddToggleThing(DropdownOption template, Transform parent, UiTooltip tooltip, string label, string description, Func<bool> get, Action<bool> set) // spawns a toggle setting using a given template and values
     {
-        Component clone = UnityEngine.Object.Instantiate((Component) template, parent);
+        Component clone = UnityEngine.Object.Instantiate(template, parent);
 
-        SetLabel(clone, label);
+        clone.name = $"CT_{label}";
+        foreach (var localizComp in clone.GetComponentsInChildren<LocalizeStringEvent>(true)) localizComp.enabled = false; // disable localization components
 
         DropdownOption dropdown = clone.GetComponent<DropdownOption>();
+        if (dropdown == null) return;
 
         dropdown.SetOptions(boolSetting);
         dropdown.Initialize(() => set(dropdown.value == 0), get() ? 0 : 1);
-        tooltip.RegisterTooltip(clone.GetComponent<RectTransform>(), description);
-    }
 
-    private static void SetLabel(Component root, string text)
-    {
-        foreach (var MeshText in root.GetComponentsInChildren<TMP_Text>(true))
+        foreach (var meshText in clone.GetComponentsInChildren<TMP_Text>(true))
         {
-            if (MeshText.GetComponentInParent<TMP_Dropdown>() != null) continue;
+            if (meshText.GetComponentInParent<TMP_Dropdown>() != null) continue;
 
-            MeshText.text = text;
-            return;
+            meshText.text = label; // set row title
+            meshText.ForceMeshUpdate();
+
+            break;
         }
+
+        tooltip.RegisterTooltip(clone.GetComponent<RectTransform>(), description);
     }
 }
